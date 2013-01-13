7cd3199a5bdbdf9d72d26f8957071365b8d9cccd
Jesse Latham <=>
2013-01-13 13:29:33 -0800

    fancied up index and details pages for portfolios, rooted portfolios/ to home, initial bits of test data

--- portfolioapp/urls.py
+++ portfolioapp/urls.py
@@ -4,14 +4,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^portfolios/?', include('portfolios.urls')),
-
-    # Examples:
-    # url(r'^$', 'portfolioapp.views.home', name='home'),
-    # url(r'^portfolioapp/', include('portfolioapp.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^/?', include('portfolios.urls')),
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
