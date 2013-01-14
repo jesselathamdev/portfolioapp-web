78f828603136cca6d35b406ca30166fd8032a18f
Jesse Latham <=>
2013-01-13 20:59:26 -0800

    added home app

--- portfolioapp/urls.py
+++ portfolioapp/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import patterns, include, url
 
+from home import views
+
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^/?', include('portfolios.urls')),
+    url(r'^$', views.index, name="home"),
+    url(r'^portfolios/?', include('portfolios.urls')),
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
--- portfolios/urls.py
+++ portfolios/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import patterns, url
+
 from portfolios import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'),
+    url(r'^/?', views.index, name='index'),
     url(r'^(?P<portfolio_id>\d+)/?', views.detail, name='detail'),
 )
