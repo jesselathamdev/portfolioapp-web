d3e3408ee8689e378c9d0e8856636d635bfdd20c
Jesse Latham <=>
2013-03-12 23:21:00 -0700

    api v2: GET markets, now with body header

--- portfolioapp/apps/api/v2_api.py
+++ portfolioapp/apps/api/v2_api.py
@@ -1,7 +1,29 @@
 # api/v2_api.py
+import json
+from django.core.serializers.json import DjangoJSONEncoder
 from django.http import HttpResponse
 
 from portfolioapp.apps.portfolios.models import Portfolio
+from portfolioapp.apps.markets.models import Market
 
 def get_portfolios(request):
-    return HttpResponse('hello', mimetype='application/json')
+    if request.method == 'GET':
+        portfolios = list(Portfolio.objects.filter(user_id=2).values('id', 'name', 'date_created'))
+        json_portfolios = json.dumps(portfolios, cls=DjangoJSONEncoder)
+        return HttpResponse(json_portfolios, mimetype='application/json', status=200)
+
+def get_markets(request):
+    if request.method == 'GET':
+        markets = list(Market.objects.all().values('id', 'name', 'date_created'))
+        results = {}
+        results['data'] = markets
+        return HttpResponse(json.dumps(results, cls=DjangoJSONEncoder), mimetype='application/json', status=200)
+
+
+class JSONResponseJSONDumps(HttpResponse):
+    """JSON response class."""
+    def __init__(self, obj='', json_opts={}, mimetype="application/json", *args, **kwargs):
+         content = json.dumps(obj, cls=DjangoJSONEncoder)
+         super(JSONResponseJSONDumps, self).__init__(content, mimetype, *args, **kwargs)
+
+# http://stackoverflow.com/questions/2428092/creating-a-json-response-using-django-and-python
--- portfolioapp/apps/api/v2_urls.py
+++ portfolioapp/apps/api/v2_urls.py
@@ -1,7 +1,8 @@
 # api/v2_urls.py
 from django.conf.urls import patterns, url
-from .v2_api import get_portfolios
+from .v2_api import get_portfolios, get_markets
 
 urlpatterns = patterns('',
     url(r'^portfolios/', get_portfolios),
+    url(r'^markets/', get_markets),
 )
