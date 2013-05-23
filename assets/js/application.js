ca8a103eb9100e79e41f60da334914eeeccce5d3
Jesse Latham <jesse.latham.dev@gmail.com>
2013-05-22 18:58:37 -0700

    fixed bug where fixed to floating header didn't play nice with the new template separation, jumping ensued when the breadcrumb bar was just peaking or not present at all

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,22 +1,33 @@
 $(function() {
-  // Setup drop down menu
-  $('.dropdown-toggle').dropdown();
+    // Setup drop down menu
+    $('.dropdown-toggle').dropdown();
 
-  // Fix input element click problem
-  $('.login-dropdown, #btn-login').click(function(e) {
-    e.stopPropagation();
-  });
+    // Fix input element click problem
+    $('.login-dropdown, #btn-login').click(function(e) {
+        e.stopPropagation();
+    });
 });
 
 // For floating/following main header on long pages
+var distanceToTop = 0;
 var fixed = false;
 
+$(document).ready(function(){
+    distanceToTop = $('#bottom').offset().top;  // 112px for unauth'd home, 128px for auth'd dashboard, 148px for auth'd page with breadcrumbs
+});
+
 $(document).scroll(function() {
-    if ($(this).scrollTop() > 70) {
-        if( !fixed ) {
+    if ($(this).scrollTop() > 70) {  // this is the height of the header above the main navigation
+        if(!fixed) {
             fixed = true;
             $('#fixed-nav').addClass('fixed-navbar');
-            $('#bottom').css('marginTop', '78px');
+            if (distanceToTop == 112) {
+                $('#bottom').css('marginTop', '41px');  // 78px  // 58px for dashboard page // 41px for unauth'd home
+            } else if (distanceToTop == 128) {
+                $('#bottom').css('marginTop', '58px');
+            } else if (distanceToTop == 148) {
+                $('#bottom').css('marginTop', '78px');
+            }
         }
     } else {
         if(fixed) {
