2d149e4c088cfea3d4bd1041b12914918c0a608d
Jesse Latham <jesse.latham.dev@gmail.com>
2013-05-25 18:02:53 -0700

    added coverage module back in for test reporting

--- portfolioapp/tests/site/views_test.py
+++ portfolioapp/tests/site/views_test.py
@@ -13,14 +13,17 @@ class TestSiteViews(object):
 
     # test for existence of some common pages
 
-    def test_home_page(self):
+    def test_view_home_page_succeed(self):
+        print('in view_test_home_page_succeed')
         response = self.client.get('/')
         nt.assert_equal(response.status_code, 200)
 
-    def test_signup_page(self):
+    def test_view_signup_page_succeed(self):
+        print('in view_test_signup_page_succeed')
         response = self.client.get('/sign-up')
         nt.assert_equal(response.status_code, 301)
 
-    def test_signin_page(self):
+    def test_view_signin_page_succeed(self):
+        print('in view_test_signin_page_succeed')
         response = self.client.get('/sign-in')
         nt.assert_equal(response.status_code, 301)
