227681cc075086ec4f44ff461a3ed56c6a15862b
Jesse Latham <jesse.latham.dev@gmail.com>
2013-05-26 16:00:11 -0700

    added historical prices list page at /stocks/:id/prices; BNS:TSX (948) and BB:TSX (6268) have sample data

--- portfolioapp/apps/markets/urls.py
+++ portfolioapp/apps/markets/urls.py
@@ -6,4 +6,5 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('',
     url(r'^$', 'portfolioapp.apps.markets.views.stock_index', name='markets_stocks_index'),
     url(r'^(?P<stock_id>\d+)/$', 'portfolioapp.apps.markets.views.stock_show', name='markets_stocks_show'),
+    url(r'^(?P<stock_id>\d+)/prices/$', 'portfolioapp.apps.markets.views.stock_historical_prices_index', name='markets_stocks_historical_prices_index'),
 )
--- portfolioapp/apps/markets/views.py
+++ portfolioapp/apps/markets/views.py
@@ -7,7 +7,7 @@ from django.db.models import Q
 from endless_pagination.decorators import page_template
 
 from portfolioapp.apps.core import settings
-from .models import Stock
+from .models import Stock, StockPriceHistory
 from .forms import StockSearchForm
 
 
@@ -23,19 +23,10 @@ def stock_index(request, template='markets/stocks/index.html', extra_context=Non
             stock_id = form.cleaned_data['stock_id']
             search_term = form.cleaned_data['search_term']
 
-            print('form is valid')
-
             if stock_id:
-                print('found stock id')
                 stocks = Stock.objects.select_related('stock__name', 'stock__symbol', 'market__acr').filter(id=stock_id).order_by('name')
             elif search_term:
-                print('found search term')
                 stocks = Stock.objects.select_related('stock__name', 'stock__symbol', 'market__acr').filter(Q(name__icontains = search_term) | Q(symbol__icontains = search_term)).order_by('name')
-            else:
-                print('stock_id: %s; search_term: %s' % (stock_id, search_term))
-
-        else:
-            print('error: %s' % form.errors)
 
     else:
         form = StockSearchForm()
@@ -53,3 +44,17 @@ def stock_show(request, stock_id):
     stock = get_object_or_404(Stock, pk=stock_id)
 
     return render(request, 'markets/stocks/show.html', {'stock': stock})
+
+
+@login_required
+@page_template('markets/stocks/historical_prices_index_paged_content.html')
+def stock_historical_prices_index(request, stock_id, template='markets/stocks/historical_prices_index.html', extra_context=None):
+    stock = get_object_or_404(Stock.objects.select_related('stock__name', 'stock__symbol', 'market__acr'), pk=stock_id)
+    prices = StockPriceHistory.objects.filter(stock_id=stock_id).order_by('-date_created')
+
+    context = {'stock': stock, 'prices': prices, 'results_per_page': settings.RESULTS_PER_PAGE}
+
+    if extra_context is not None:
+        context.update(extra_context)
+
+    return render_to_response(template, context, context_instance=RequestContext(request))
